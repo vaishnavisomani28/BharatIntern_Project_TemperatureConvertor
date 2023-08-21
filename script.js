56842aef138cac0fcfee8c4b4382d8c6b5907b7e
Vaishnavi Somani <66909940+vaishnavisomani28@users.noreply.github.com>
2023-08-21 21:47:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const convertBtn = document.getElementById("convertBtn");
+
+convertBtn.addEventListener("click", () => {
+  const celsiusInput = document.getElementById("celsius");
+  const fahrenheitInput = document.getElementById("fahrenheit");
+
+  if (celsiusInput.value !== "") {
+    const celsius = parseFloat(celsiusInput.value);
+    const fahrenheit = (celsius * 9/5) + 32;
+    fahrenheitInput.value = fahrenheit.toFixed(2);
+  } else if (fahrenheitInput.value !== "") {
+    const fahrenheit = parseFloat(fahrenheitInput.value);
+    const celsius = (fahrenheit - 32) * 5/9;
+    celsiusInput.value = celsius.toFixed(2);
+  }
+});
